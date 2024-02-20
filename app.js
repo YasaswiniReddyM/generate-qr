8704114d8d1a7974650974f8176ef915cea28521
Yasaswini Reddy M <yasaswinireddym@gmail.com>
2024-02-20 14:27:49 +0530

    Necessary Changes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const swaggerSpec = require('./swagger-config.js');
 const mongoose = require('mongoose');
 const axios = require('axios');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
